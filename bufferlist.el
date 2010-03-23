54d9107a6008bafc9e31c2eb4b35a647c4d8165f
Robert Lillack <rlillack@yasni.de>
2010-03-23 15:56:26 +0100

    Delete open buffers from bufferlist

--- bufferlist.el
+++ bufferlist.el
@@ -4,6 +4,10 @@
   (let ((map (make-sparse-keymap)))
     (define-key map [f3] 'kill-buffer-and-window)
     (define-key map [return] 'bufferlist-activate-buffer)
+    (define-key map [delete] 'bufferlist-kill-buffer)
+    (define-key map [deletechar] 'bufferlist-kill-buffer)
+    (define-key map [kp-delete] 'bufferlist-kill-buffer)
+    (define-key map [backspace] 'bufferlist-kill-buffer)
     (define-key map [up] 'bufferlist-move-up)
     (define-key map [down] 'bufferlist-move-down)
     map))
@@ -24,6 +28,17 @@
     (message "Switching to %s" name)
     (bufferlist-quit)
     (switch-to-buffer buf)))
+
+(defun bufferlist-kill-buffer ()
+  ""
+  (interactive)
+  (let* ((bufinfo (nth (+ -1 (line-number-at-pos)) bufferlist-list))
+        (name (nth 1 bufinfo))
+        (buf (nth 0 bufinfo)))
+    (message "Killing %s" name)
+    (kill-buffer buf)
+    (bufferlist-load-buffers)))
+
 
 (defun bufferlist-style-buffer ()
   (setq buffer-read-only nil)
@@ -58,66 +73,64 @@
   (bufferlist-style-buffer)
 )
 
+(defun bufferlist-load-buffers (&optional active-buffer)
+  (setq bufferlist-list '())
+                                        ; buffer-liste holen
+  (dolist (b (buffer-list))
+    (with-current-buffer b
+      (let ((name (buffer-name))
+            (file buffer-file-name))
+        (unless (and (null file)
+                     (or (string= "BUFFERLIST" name)
+                         (string= (substring name 0 1) " ")))
+          (push (list b name file (buffer-modified-p))
+                bufferlist-list)))))
+                                        ; nach namen sortieren
+  (setq bufferlist-list
+        (sort bufferlist-list (lambda (a b) (string< (nth 1 a) (nth 1 b)))))
+
+  (with-current-buffer (get-buffer-create "BUFFERLIST")
+    (setq buffer-read-only nil)
+    (erase-buffer)
+    (goto-char (point-min))
+                                        ; hier schon mal mark setzen, falls es spaeter
+                                        ; nicht gesetzt wird.
+    (setq mark (point-min))
+    (dolist (e bufferlist-list)
+      (insert (with-output-to-string
+                (when (and active-buffer
+                           (eq (nth 0 e) active-buffer))
+                  (setq mark (point)))
+                (princ " ")
+                (princ (nth 1 e))
+                (when (nth 3 e)
+                  (princ "+"))
+                (when (and active-buffer
+                           (eq (nth 0 e) active-buffer))
+                  (princ "*"))
+                (princ "\n"))))
+    (setq major-mode 'bufferlist-mode)
+    (setq mode-name "BUFFERLIST")
+    (use-local-map bufferlist-mode-map)
+                                        ;(setq bufferlist-list list)
+    (goto-char mark)
+    (bufferlist-style-buffer)
+                                        ;(defface bufferlist-selected '((t :background "#ff0000" :foreground "white")) nil)
+                                        ;(setq font-lock-keywords nil)
+                                        ;(setq font-lock-defaults '('(("^>.*$" . bufferlist-selected))
+                                        ;                           ("\\*.*\\*" . font-lock-comment-face)))
+                                        ;(font-lock-fontify-buffer)
+    (set-buffer-modified-p nil)
+    (setq buffer-read-only t)
+    (switch-to-buffer (current-buffer))))
+
 (defun bufferlist ()
   "erstellt eine bufferlist aehnlich der, die ich fuer vim gebaut habe "
   (interactive)
-  (setq bufferlist-list '())
   (setq bufferlist-background nil)
   ; temporarily turn off global hl-line-mode
   (when global-hl-line-mode
     (setq bufferlist-global-hl-line-mode t)
     (global-hl-line-mode -1))
-  (let ((oldbuf (current-buffer))
-        (buf (get-buffer-create "BUFFERLIST"))
-        mark)
-    (split-window-horizontally 30)
-    ; buffer-liste holen
-    (dolist (b (buffer-list))
-      (with-current-buffer b
-        (let ((name (buffer-name))
-              (file buffer-file-name))
-          (unless (or (eq b buf)
-                      (and (null file)
-                           (string= (substring name 0 1)
-                                    " ")))
-            (push (list b name file (buffer-modified-p))
-                  bufferlist-list)))))
-    ; nach namen sortieren
-    (setq bufferlist-list
-          (sort bufferlist-list (lambda (a b) (string< (nth 1 a) (nth 1 b)))))
-    (with-current-buffer buf
-      (setq buffer-read-only nil)
-      (erase-buffer)
-      (goto-char (point-min))
-      ; hier schon mal mark setzen, falls es spaeter
-      ; nicht gesetzt wird.
-      (setq mark (point-min))
-      (dolist (e bufferlist-list)
-        (insert (with-output-to-string
-                  (if (eq (nth 0 e) oldbuf)
-                      (setq mark (point)))
-                  (princ " ")
-                  (princ (nth 1 e))
-                  (when (nth 3 e) (princ "+"))
-                  (when (eq (nth 0 e) oldbuf)
-                    (princ "*"))
-                  (princ "\n")))
-        )
-      (setq major-mode 'bufferlist-mode)
-      (setq mode-name "BUFFERLIST")
-      (use-local-map bufferlist-mode-map)
-      ;(setq bufferlist-list list)
-      (goto-char mark)
-      (bufferlist-style-buffer)
-      ;(defface bufferlist-selected '((t :background "#ff0000" :foreground "white")) nil)
-      ;(setq font-lock-keywords nil)
-      ;(setq font-lock-defaults '('(("^>.*$" . bufferlist-selected))
-      ;                           ("\\*.*\\*" . font-lock-comment-face)))
-      ;(font-lock-fontify-buffer)
-      (set-buffer-modified-p nil)
-      (setq buffer-read-only t)
-      )
-    ;(hl-line-mode nil)
-    (switch-to-buffer buf))
-;    (delete-other-windows)
-)
+  (split-window-horizontally 30)
+  (bufferlist-load-buffers (current-buffer)))
